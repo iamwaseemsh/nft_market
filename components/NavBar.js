aee7362a489306f6f11095638c1ec618340e6138
Muhammad Waseem <iamwaseemsh@gmail.com>
2022-02-27 13:06:00 +0500

    changes made 1

--- components/NavBar.js
+++ components/NavBar.js
@@ -24,12 +24,12 @@ import Link from 'next/link'
       <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
         <div className="logo flex-shrink-0 flex items-center">
         <div className="flex-shrink-0 flex items-center">
-          <img className="block lg:hidden h-8 w-auto" src="https://ardapps.com/wp-content/uploads/2021/11/icon-2.png" alt="Workflow"/>
-          <img className="hidden lg:block h-8 w-auto" src="https://ardapps.com/wp-content/uploads/2021/11/icon-2.png" alt="Workflow"/>
+          <img className="block lg:hidden h-8 w-auto" src="icons/parsar-logo.png" alt="Workflow"/>
+          <img className="hidden lg:block h-8 w-auto" src="icons/parsar-logo.png" alt="Workflow"/>
         </div>
         <div className="logoName">
             <Link  href="/">
-            <a className="text-yellow-500 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-bold">Arabic NFTs</a>
+            <a className="text-yellow-500 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-bold"></a>
             </Link>
             </div>
 
